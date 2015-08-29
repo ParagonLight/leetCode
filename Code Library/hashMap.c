a4b743c13209e8556769f1a41ee5d184567101e2
paragonlight <jingwei.xu.nju@gmail.com>
2015-08-29 16:22:38 -0500

    create code library folder, and add hashMap when key and value are both integers

---
+++ Code Library/hashMap.c
@@ -0,0 +1,65 @@
+typedef struct HashNode{
+    int val;
+    int key;
+} HashNode;
+
+typedef struct HashMap{
+    int size;
+    HashNode** storage;
+} HashMap;
+
+HashMap* hash_create(int size);
+void hash_destroy(HashMap* hashMap);
+void hash_set(HashMap* hashMap, int key, int value);
+HashNode* hash_get(HashMap* hashMap, int key);
+
+HashMap* hash_create(int size){
+    HashMap* hashMap = malloc(sizeof(HashMap));
+    hashMap->size = size;
+    hashMap->storage = calloc(size, sizeof(HashNode*));
+    return hashMap;
+}
+
+void hash_destroy(HashMap* hashMap){
+    for(int i = 0; i < hashMap->size; i ++){
+        HashNode* node = hashMap->storage[i];
+        if(node){
+            free(node);
+        }
+    }
+    free(hashMap->storage);
+    free(hashMap);
+}
+
+void hash_set(HashMap* hashMap, int key, int value){
+    int hash = abs(key) % hashMap->size;
+    HashNode* node;
+    while((node = hashMap->storage[hash])){
+        if(hash < hashMap->size - 1){
+            hash ++;
+        }else{
+            hash = 0;
+        }
+    }
+    
+    node = malloc(sizeof(hashNode));
+    node->val = value;
+    node->key = key;
+    hashMap->storage[hash] = node;
+}
+
+HashNode* hash_get(HashMap* hashMap, int key){
+    int hash = abs(key) % hashMap->size;
+    HashNode* node;
+    while((node = hashMap->storage[hash])){
+        if(node->key == key){
+            return node;
+        }
+        if(hash < hashMap->size - 1){
+            hash ++;
+        }else{
+            hash = 0;
+        }
+    }
+    return NULL;
+}
